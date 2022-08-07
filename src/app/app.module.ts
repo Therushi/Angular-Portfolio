9b8a95bc2b06c7262bd3b90b6507649a62b6e9d3
Rushikesh Dhanawade <rushidhanawade123@gmail.com>
2022-08-07 17:51:07 +0530

    added projects component & dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
